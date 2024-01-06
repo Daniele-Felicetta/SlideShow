e70ca54283053dae5e06f42012c016882c882453
Daniele Felicetta <felicettadaniele@gmail.com>
2024-01-06 23:06:51 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="my-root">
+    <v-card >
+      <v-card-title>Inserisci i file</v-card-title>
+      <v-card-subtitle>Assicurati che i tuoi file siano solo immagini</v-card-subtitle>
+    </v-card>
+
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  .my-root{
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
