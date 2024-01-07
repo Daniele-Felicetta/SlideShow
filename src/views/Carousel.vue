bbe195b5ee25fd6eaa5bbec29b7f52d2d924d777
Daniele Felicetta <felicettaweb@gmail.com>
2024-01-07 16:09:47 +0100

    set enviroment

---
+++ src/views/Carousel.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Carousel</h1>
+  <div>
+    <v-carousel progress cycle>
+      <v-carousel-item v-for="image in images"></v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+<script setup>
+const props=defineProps(['images'])
+</script>
+<style>
+  
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,45 @@
 <template>
-  <div class="my-root">
-    <v-card >
-      <v-card-title>Inserisci i file</v-card-title>
-      <v-card-subtitle>Assicurati che i tuoi file siano solo immagini</v-card-subtitle>
-    </v-card>
+  <div class="my-root ">
+    <div v-if="!start">
+      <v-card class="pa-10 d-flex flex-column">
+        <v-card-title>Inserisci i file</v-card-title>
+        <v-card-subtitle>Assicurati che i tuoi file siano solo immagini</v-card-subtitle>
+        <v-file-input class="pt-8" multiple prepend-icon="mdi-image" v-model="images"></v-file-input>
+        <v-btn class="align-self-md-end" @click="() => start = true">Go <v-icon>mdi-arrow-right-drop-circle</v-icon>
+        </v-btn>
+      </v-card>
+    </div>
 
+    <div class="carousel" v-else>
+      <v-btn class="close" icon="mdi-exit-run" @click="() => start= false"></v-btn>
+      <Carousel images=images></Carousel>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import Carousel from './Carousel.vue';
+const start = ref(false)
+const images = ref([])
+
 </script>
 
 <style scoped>
-  .my-root{
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+.my-root {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.carousel {
+  
+}
+
+.close {
+  position: absolute;
+  top:2rem;
+  right: 5rem;
+}
 </style>
