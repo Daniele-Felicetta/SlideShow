4de548a1c1fca85a38070c3890e29f551eada4cf
Daniele Felicetta <felicettaweb@gmail.com>
2024-01-26 16:03:05 +0100

    image import and carousel view

--- src/views/Carousel.vue
+++ src/views/Carousel.vue
@@ -1,14 +1,51 @@
 <template>
-  <h1>Carousel</h1>
-  <div>
-    <v-carousel progress cycle>
-      <v-carousel-item v-for="image in images"></v-carousel-item>
+  <div :class="{'fullScreen':fullScreen}">
+    <v-carousel :class="{ 'carousel': fullScreen }" progress cycle >
+      <v-carousel-item class="carousel-item" v-for="base in base64">
+        <img :src="base" alt="">
+      </v-carousel-item>
     </v-carousel>
-  </div>
+      <v-btn class="fullScreenButton" @click="handleFullScreen" >FullScreen</v-btn>
+  </div> 
+  <br>
 </template>
 <script setup>
-const props=defineProps(['images'])
+import { onMounted, ref } from 'vue'
+const props = defineProps(['base64']);
+const emits=defineEmits(['fullScreen'])
+const fullScreen = ref(false)
+function handleFullScreen() {
+  fullScreen.value = !fullScreen.value
+  emits('fullScreen');
+}
 </script>
-<style>
-  
+<style scoped>
+.carousel{
+  height: 95vh!important;
+}
+
+.fullScreenButton{
+  position: absolute;
+  bottom: 5%;
+  right: 5%;
+}
+.fullScreen{
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top:2%;
+  left: 2%;
+  width: 95vw;
+  height: 95vh;
+
+}
+.carousel-item{
+}
+img{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,79 @@
 <template>
-  <div class="my-root ">
-    <div v-if="!start">
-      <v-card class="pa-10 d-flex flex-column">
+  <div  class="my-root">
+    <div :class="{ 'hide': fullScreen }" v-if="!start">
+      <v-card class="cardInput pa-10 d-flex flex-column">
         <v-card-title>Inserisci i file</v-card-title>
         <v-card-subtitle>Assicurati che i tuoi file siano solo immagini</v-card-subtitle>
-        <v-file-input class="pt-8" multiple prepend-icon="mdi-image" v-model="images"></v-file-input>
-        <v-btn class="align-self-md-end" @click="() => start = true">Go<v-icon>mdi-arrow-right-drop-circle</v-icon></v-btn>
+        <v-file-input class="input pt-8" ref="imageInput" @change="(e) => handleSelected(e)" multiple prepend-icon="mdi-image"
+          v-model="images"></v-file-input>
+        <v-btn class="align-self-md-end"
+          @click="() => start = true">Go<v-icon>mdi-arrow-right-drop-circle</v-icon></v-btn>
       </v-card>
     </div>
 
-    <div class="carousel" v-else>
-      <v-btn class="close" icon="mdi-exit-run" @click="() => start= false"></v-btn>
-      <Carousel images=images></Carousel>
+    <div :class="{ 'z-idx': fullScreen }" class="carousel" v-else>
+      <v-btn class="close" icon="mdi-exit-run" @click="() => start = false"></v-btn>
+      <Carousel @fullScreen="handleFullScreen" :base64="base64" :start="start" :imageInput="imageInput" :images=images></Carousel>
     </div>
-    <v-btn class="showFrameButton" icon="mdi-fullscreen-exit" @click="() => showFrame = !showFrame"></v-btn>
-    <div v-if="showFrame">
-      <iframe class="framePage" :class="{framePageMinus:resizeFrame}" src="https://vuetifyjs.com/en/getting-started/installation/#installation" frameborder="20"></iframe>
-      <v-btn @click="() => resizeFrame = !resizeFrame">{{resizeFrame  ? "Relarge" : "Reduce size"}}</v-btn>
+
+    <div :class="{ 'hide': fullScreen }">
+      <v-btn  class="showFrameButton d-none d-lg-flex" icon="mdi-fullscreen-exit"
+      @click="() => showFrame = !showFrame"></v-btn>
+      <div class="d-flex flex-column ml-8" :class="{ 'hide': !showFrame }">
+        <iframe class="d-none d-lg-flex framePage" :class="{ framePageMinus: resizeFrame }"
+        src="https://vuetifyjs.com/en/getting-started/installation/#installation" frameborder="20"></iframe>
+        <v-btn @click="() => resizeFrame = !resizeFrame">{{ resizeFrame ? "Reduce" :  "Larger" }}</v-btn>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import Carousel from './Carousel.vue';
-const start = ref(false)
-const images = ref([])
-const showFrame = ref(false)
-const resizeFrame = ref(false)
+const start = ref(false);
+const images = ref([]);
+const showFrame = ref(false);
+const resizeFrame = ref(false);
+const imageInput = ref(null);
+const base64 = ref([]);
+const fullScreen = ref(false);
+
+function handleFullScreen(){
+  fullScreen.value = true;
+}
+
+function handleSelected(e) {
+  console.log('ciao')
+  for (let i = 0; i < imageInput.value.files.length; i++) {
+    const reader = new FileReader();
+    const selectedFile = imageInput.value.files[i];
+    if (selectedFile) {
+      reader.onloadend = (evt) => {
+        base64.value[i] = reader.result;
+        console.log(base64.value[i]);
+      }
+      reader.readAsDataURL(selectedFile);
+    }
+  }
+}
 </script>
 
 <style scoped>
+.cardInput{
+  max-width: 600px;
+  width: 500px;
+}
+@media screen and (max-width: 500px){
+  .cardInput{
+    max-width: 300px;
+    width: 250px;
+  }
+}
+.input{
+  word-break: break-all;
+  
+}
 .my-root {
   height: 100vh;
   display: flex;
@@ -39,25 +82,42 @@ const resizeFrame = ref(false)
 }
 
 .carousel {
-  
+  width: 60vw;
+  display: block !important;
 }
 
 .close {
   position: absolute;
-  top:2rem;
+  top: 2rem;
   right: 5rem;
 }
-.showFrameButton{
+
+.hide {
+  display: none !important;
+}
+
+.showFrameButton {
   position: absolute;
-  top:2rem;
+  top: 2rem;
   left: 5rem;
 }
-.framePage{
-  width: 1450px;
+
+.framePage {
+width: 40vw;
   height: 800px;
 }
-.framePageMinus{
-  width: 1000px;
+
+.framePageMinus {
+  width: 60vw;
   height: 800px;
+
+}
+</style>
+<style>
+.z-idx{
+  z-index: 999;
+}
+button{
+  z-index: inherit;
 }
 </style>
